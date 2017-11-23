fd020961d76fb71559feed9c4d11d8cbf7c6e0c9
Pratiman Shahi <spratiman@yahoo.com>
2017-11-23 17:47:38 -0500

    Milestone 1 FSM added

--- plotfour_top.do
+++ plotfour_top.do
@@ -8,8 +8,15 @@ log {/*}
 add wave {/*}
 
 #0:  Nothing should be displayed on HEX0, HEX1, HEX2, HEX3, LEDR1, LEDR2, LEDR3, LEDR4
-#20: HEX0: 1, LEDR1: 1 and Nothing should be displayed on HEX1, HEX2, HEX3, LEDR2, LEDR3, LEDR4
-#30: 
-force {KEY}   0000 0,   0100 20 -r 60
-force {SW} 	000000 0, 000001 20 -r 20
-run 20ns
+#20: HEX0: 1 and Nothing should be displayed on HEX1, HEX2, HEX3, LEDR1, LEDR2, LEDR3, LEDR4
+#40: HEX0: 1, LEDR1: 1 and Nothing should be displayed on HEX1, HEX2, HEX3, LEDR2, LEDR3, LEDR4
+#60: HEX0: 5, LEDR1: 1 and Nothing should be displayed on HEX1, HEX2, HEX3, LEDR2, LEDR3, LEDR4
+#80: HEX0: 9, LEDR1: 1 and Nothing should be displayed on HEX1, HEX2, HEX3, LEDR2, LEDR3, LEDR4
+#100: HEX0: D, LEDR1: 1 and Nothing should be displayed on HEX1, HEX2, HEX3, LEDR2, LEDR3, LEDR4
+#120: HEX0: 1, HEX2: 1, LEDR1: 1, LEDR3: 1 and Nothing should be displayed on HEX2, HEX3, LEDR2, LEDR4
+
+force {KEY}   0000 0, 1100 20, 1101 40, 1100 59, 1101 60, 1100 79, 1101 80, 1100 99, 1101 100, 1100 119, 1101 120 -r 140
+force {SW} 	000000 0, 000001 20, 000001 40, 000101 60, 001001 80, 001101 100, 010001 120 -r 140
+#force {CLOCK_50} 1 0, 0 21, 1 22, 0 41, 1 42, 0 61, 1 62 -r 140
+
+run 140ns
--- plotfour_top.v
+++ plotfour_top.v
@@ -1,54 +1,64 @@
 
 module plotfour_top (
-	CLOCK _50,								// On Board 50 MHz
+	CLOCK_50,								// On Board 50 MHz
 	KEY,									// KEY[3]: START, KEY[1]: P_one, KEY[2]: P_two, KEY[0]: resetn 
 	SW,										// SW[0:5]: Input square number
+	HEX0,
+	HEX1,
+	HEX2,
+	HEX3,
+	LEDR
 	// The ports below are for the VGA output.  Do not change.
-	VGA_CLK,   								//	VGA Clock
-	VGA_HS,									//	VGA H_SYNC
-	VGA_VS,									//	VGA V_SYNC
-	VGA_BLANK_N,							//	VGA BLANK
-	VGA_SYNC_N,								//	VGA SYNC
-	VGA_R,   								//	VGA Red[9:0]
-	VGA_G,	 								//	VGA Green[9:0]
-	VGA_B   								//	VGA Blue[9:0]
+	//VGA_CLK,   								//	VGA Clock
+	//VGA_HS,									//	VGA H_SYNC
+	//VGA_VS,									//	VGA V_SYNC
+	//VGA_BLANK_N,							//	VGA BLANK
+	//VGA_SYNC_N,								//	VGA SYNC
+	//VGA_R,   								//	VGA Red[9:0]
+	//VGA_G,	 								//	VGA Green[9:0]
+	//VGA_B   								//	VGA Blue[9:0] 
 	);
 	
 	input 			CLOCK_50;				//50 MHz
-	input [5:0] 	SW;						// SW[5:0]: Square number
-	input [2:0] 	KEY;					// KEY[3]: START, KEY[1]: p_one, KEY[2]: p_two, KEY[0]: Reset
+	input  [5:0] 	SW;						// SW[5:0]: Square number
+	input  [0:3] 	KEY;					// KEY[3]: START, KEY[1]: p_one, KEY[2]: p_two, KEY[0]: Reset
 	//P_one square selection
-	output [6:0] 	HEX0;
-	output [6:0] 	HEX1;
+	output  [6:0] 	HEX0;
+	output  [6:0] 	HEX1;
 	//P_two square selection
-	output [6:0] 	HEX2;
-	output [6:0] 	HEX3;
-	output [4:0] 	LEDR;					// LEDR[1]: p_one turn, LEDR[2]: p_two turn, LEDR[3]: p_one_win, LEDR[4]: p_two_win
+	output  [6:0] 	HEX2;
+	output  [6:0] 	HEX3;
+	output  [0:4] 	LEDR;					// LEDR[1]: p_one turn, LEDR[2]: p_two turn, LEDR[3]: p_one_win, LEDR[4]: p_two_win
 	
 	// Do not change the following outputs
-	output			VGA_CLK;   				//	VGA Clock      
+	/*output			VGA_CLK;   				//	VGA Clock      
 	output			VGA_HS;					//	VGA H_SYNC
 	output			VGA_VS;					//	VGA V_SYNC
 	output			VGA_BLANK_N;			//	VGA BLANK
 	output			VGA_SYNC_N;				//	VGA SYNC
 	output	[9:0]	VGA_R;   				//	VGA Red[9:0]
 	output	[9:0]	VGA_G;	 				//	VGA Green[9:0]
-	output	[9:0]	VGA_B;   				//	VGA Blue[9:0]
+	output	[9:0]	VGA_B;   				//	VGA Blue[9:0] */
 	
-	wire resetn;
+	wire resetn, start, p_one, p_two, p_one_win, p_two_win;
 	assign resetn = KEY[0];
+	assign start = KEY[3];
+	assign p_one = KEY[1];
+	assign p_two = KEY[2];
+	assign p_one_win = LEDR[3];
+	assign p_two_win = LEDR[4];
 	
 	// Create the colour, x, y and writeEn wires that are inputs to the controller.
-	wire [2:0] colour;
+	/*wire [2:0] colour;
 	wire [7:0] x;
 	wire [6:0] y;
 	wire writeEn;
-	wire enable,ld_x,ld_y,ld_c;
+	wire enable,ld_x,ld_y,ld_c;*/
 	
 	// Create an Instance of a VGA controller - there can be only one!
 	// Define the number of colours as well as the initial background
 	// image file (.MIF) for the controller.
-	vga_adapter VGA(
+	/*vga_adapter VGA(
 			.resetn(resetn),
 			.clock(CLOCK_50),
 			.colour(colour),
@@ -56,7 +66,7 @@ module plotfour_top (
 			.y(y),
 			.plot(writeEn),
 			/* Signals for the DAC to drive the monitor. */
-			.VGA_R(VGA_R),
+			/*.VGA_R(VGA_R),
 			.VGA_G(VGA_G),
 			.VGA_B(VGA_B),
 			.VGA_HS(VGA_HS),
@@ -67,66 +77,93 @@ module plotfour_top (
 		defparam VGA.RESOLUTION = "160x120";
 		defparam VGA.MONOCHROME = "FALSE";
 		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
-		defparam VGA.BACKGROUND_IMAGE = "black.mif";
+		defparam VGA.BACKGROUND_IMAGE = "black.mif";*/
 	
-	always @(posedge clk)
-		if(KEY[1] == 1)
+	reg LEDR_status;
+	
+	always @(*) 
+	begin
+		if(start && p_one)
+			LEDR_status <= 1;
+		else if (!start && p_one)
+			LEDR_status <= 0;
+	end
+	assign LEDR[1] = LEDR_status; 
+		/*else if (KEY[2] == 1)
 		begin
-			hex_decoder h0(
-				.hex_digit(square[7:4]),
-				.segments(HEX0)
-			);
-			hex_decoder h1(
-				.hex_digit(square[3:0]),
-				.segments(HEX1)
-			);
-			LEDR[1] = 1;
-		end 
-		else if (KEY[2] == 1)
-		begin
-			hex_decoder h2(
-				.hex_digit(square[7:4]),
+			 hex_decoder h2(
+				.hex_digit(SW[7:4]),
 				.segments(HEX2)
 			);
 			hex_decoder h3(
-				.hex_digit(square[3:0]),
+				.hex_digit(SW[3:0]),
 				.segments(HEX3)
 			);
-			LEDR[2] = 2;
-		end
+			assign LEDR[2] = 1; */
+		//end
+	
+	hex_decoder h0(
+		.p_one(p_one),
+		.p_two(p_two),
+		.hex_digit(SW[5:2]),
+		.segments(HEX0)
+	);
+	hex_decoder h1(
+		.p_one(p_one),
+		.p_two(p_two),
+		.hex_digit({2'b00, SW[1:0]}),
+		.segments(HEX1)
+	);
 		
-		reg [19:0] blue_square;
-		reg [19:0] red_square;
-		reg p_turn;
+		wire [19:0] blue_square;
+		wire [19:0] red_square;
+		wire p_turn, turn_status;
 		
 		fsm_controller fsm(
 			.clk(CLOCK_50),
-			.resetn(KEY[0]),
-			.p_one(KEY[1]),
+			.resetn(resetn),
+			.p_one(p_one),
 			.p_two(KEY[2]),
-			.start(KEY[3]),
+			.start(start),
 			.square(SW[5:0]),
 			.blue(blue_square),
 			.red(red_square),
 			.turn(p_turn),
-			.p_one_win(LEDR[3]),
-			.p_two_win(LEDR[4])
+			.p_one_win(p_one_win),
+			.p_two_win(p_two_win),
+			.turn_status(turn_status)
 		);
 	
 	
 
 endmodule
 
-module fsm_controller (clk, resetn, p_one, p_two, start, square, blue, red, turn, p_one_win, p_two_win);
+module fsm_controller (clk, resetn, p_one, p_two, start, square, blue, red, turn, p_one_win, p_two_win, turn_status);
 	
-	always @(posedge clk)
-		if(start == 0)
+	input [5:0] square;
+	input clk, resetn, p_one, p_two, start;
+	output reg turn, p_one_win, p_two_win;
+	output reg [19:0] blue, red;
+	/*output reg turn_status;
+	
+	always @(*)
+	begin
+		turn_status <= 0;
+	end */
+	
+	always @(*)
+	begin
+		if(start == 0 && turn_status == 0)
 		begin
 			blue 		<= 1'b0;
 			red  		<= 1'b0;
 			p_one_win 	<= 0;
 			p_two_win   <= 0;
 			turn        <= 1;
+		end
+		else if(start == 0 && turn_status == 1)
+		begin
+			turn	<= 1;
 		end
 		else if (start == 1)
 		begin
@@ -156,8 +193,15 @@ module fsm_controller (clk, resetn, p_one, p_two, start, square, blue, red, turn
 				endcase
 			end
 		end
+			else if (!resetn)
+			begin
+				blue  <= 1'b0;
+				red   <= 1'b0;
+				turn  <= 0;
+			end 
+	end
 	
-	always @(posedge clk)
+	always @(*)
 		begin
 			if(blue[0] == 1 && blue[5] == 1 && blue[10] == 1 && blue[15] == 1)
 			begin p_one_win <= 1; end
@@ -175,11 +219,14 @@ module fsm_controller (clk, resetn, p_one, p_two, start, square, blue, red, turn
 	
 endmodule
 
-module hex_decoder(hex_digit, segments);
+module hex_decoder(p_one, p_two, hex_digit, segments);
     input [3:0] hex_digit;
     output reg [6:0] segments;
+	input p_one, p_two;
    
     always @(*)
+	if (p_one)
+	begin
         case (hex_digit)
             4'h0: segments = 7'b100_0000;
             4'h1: segments = 7'b111_1001;
@@ -199,5 +246,32 @@ module hex_decoder(hex_digit, segments);
             4'hF: segments = 7'b000_1110;   
             default: segments = 7'h7f;
         endcase
+	end
+	else
+	begin
+		segments = 7'h7f;
+	end
+	/* else if (p_two)
+	begin
+		case (hex_digit)
+			4'h0: segments = 7'b100_0000;
+            4'h1: segments = 7'b111_1001;
+            4'h2: segments = 7'b010_0100;
+            4'h3: segments = 7'b011_0000;
+            4'h4: segments = 7'b001_1001;
+            4'h5: segments = 7'b001_0010;
+            4'h6: segments = 7'b000_0010;
+            4'h7: segments = 7'b111_1000;
+            4'h8: segments = 7'b000_0000;
+            4'h9: segments = 7'b001_1000;
+            4'hA: segments = 7'b000_1000;
+            4'hB: segments = 7'b000_0011;
+            4'hC: segments = 7'b100_0110;
+            4'hD: segments = 7'b010_0001;
+            4'hE: segments = 7'b000_0110;
+            4'hF: segments = 7'b000_1110;   
+            default: segments = 7'h7f;
+        endcase
+	end */
 endmodule
 	
