6227e466c42b3ffa81432104ccf331327a1ce0eb
Pratiman Shahi <spratiman@yahoo.com>
2017-11-29 22:43:33 -0500

    Two players logic added, some win cases covered

--- plotfour_top.do
+++ plotfour_top.do
@@ -7,18 +7,18 @@ vsim plotfour_top
 log {/*}
 add wave {/*}
 
-#0:  Nothing should be displayed on HEX0, HEX1, LEDR1, LEDR2, LEDR3, LEDR4
-#20: HEX0: 1 and Nothing should be displayed on HEX1, LEDR1, LEDR2, LEDR3, LEDR4
-#40: HEX0: 1, LEDR1: 1 and Nothing should be displayed on HEX1, LEDR2, LEDR3, LEDR4
-#60: HEX0: 5, LEDR1: 1 and Nothing should be displayed on HEX1, LEDR2, LEDR3, LEDR4
-#80: HEX0: 9, LEDR1: 1 and Nothing should be displayed on HEX1, LEDR2, LEDR3, LEDR4
-#100: HEX0: D, LEDR1: 1 and Nothing should be displayed on HEX1, LEDR2, LEDR3, LEDR4
-#120: HEX0: 1, HEX2: 1, LEDR1: 1, LEDR3: 1 and Nothing should be displayed on LEDR2, LEDR4
+#0:  Nothing should be displayed on HEX0, HEX1, HEX2, HEX3, LEDR1, LEDR2, LEDR3, LEDR4
+#20: HEX0: 1 and Nothing should be displayed on HEX1, HEX2, HEX3, LEDR1, LEDR2, LEDR3, LEDR4
+#40: HEX0: 0, LEDR1: 1 and Nothing should be displayed on HEX1, HEX2, HEX3, LEDR2, LEDR3, LEDR4
+#60: HEX0: 6, LEDR1: 1 and Nothing should be displayed on HEX1, HEX2, HEX3 LEDR2, LEDR3, LEDR4
+#80: HEX0: 1, HEX1: 2 LEDR1: 1 and Nothing should be displayed on HEX2, HEX3, LEDR2, LEDR3, LEDR4
+#100: HEX0: 1, HEX1: 8, HEX2: 1, LEDR1: 1, LEDR3: 1 and Nothing should be displayed on HEX3, LEDR2, LEDR4
+#120: HEX0: 1, HEX1: 8, LEDR1: 1, LEDR3: 1 and Nothing should be displayed on HEX2, HEX3, LEDR2, LEDR4
+
 #140: HEX0: 1 and Nothing should be displayed on HEX1, LEDR1, LEDR2, LEDR3, LEDR4
 #160: HEX0: 1, LEDR2: 1 and Nothing should be displayed on HEX1, LEDR1, LEDR3, LEDR4
 
-force {KEY}   1111 0, 0010 20, 0011 40, 0010 59, 0011 60, 0010 79, 0011 80, 0010 99, 0011 100, 0010 119, 0011 120, 0100 140, 0101 160 -r 180
-force {SW} 	000000 0, 000001 20, 000001 40, 000101 60, 001001 80, 001101 100, 010001 120, 000001 140, 000001 160 -r 180
-#force {CLOCK_50} 1 0, 0 21, 1 22, 0 41, 1 42, 0 61, 1 62 -r 140
+force {KEY}   1111 0, 0010 20, 0011 40, 0010 59, 0011 60, 0010 79, 0011 80, 0010 99, 0011 100, 0010 119, 0011 120, 1111 139, 0100 140, 0101 160 -r 180
+force {SW} 	000000 0, 000001 20, 000000 40, 000110 60, 001100 80, 010010 100, 011000 120, 000001 140, 000001 160 -r 180
 
 run 180ns
--- plotfour_top.v
+++ plotfour_top.v
@@ -5,6 +5,8 @@ module plotfour_top (
 	SW,										// SW[0:5]: Input square number
 	HEX0,
 	HEX1,
+	HEX2,
+	HEX3,
 	LEDR
 	// The ports below are for the VGA output.  Do not change.
 	//VGA_CLK,   								//	VGA Clock
@@ -23,6 +25,10 @@ module plotfour_top (
 	//P square selection
 	output  [6:0] 	HEX0;
 	output  [6:0] 	HEX1;
+	//P_one_score
+	output 	[6:0]	HEX2;
+	//P-two_score
+	output 	[6:0]	HEX3;
 	output  [0:4] 	LEDR;					// LEDR[1]: p_one turn, LEDR[2]: p_two turn, LEDR[3]: p_one_win, LEDR[4]: p_two_win
 	
 	// Do not change the following outputs
@@ -119,9 +125,10 @@ module plotfour_top (
 	);
 	/* Player square selection indicator [END] */
 		
-	wire [19:0] blue_square;
-	wire [19:0] red_square;
-	wire turn;
+	wire [41:0] blue_square;
+	wire [41:0] red_square;
+	wire turn; 
+	wire [3:0] p_one_score, p_two_score;
 		
 	fsm_controller fsm(
 		.resetn(resetn),
@@ -133,18 +140,26 @@ module plotfour_top (
 		.p_two_win(LEDR[4]),
 		.blue(blue_square),
 		.red(red_square),
-		.turn(turn)
+		.turn(turn),
+		.p_one_score(p_one_score),
+		.p_two_score(p_two_score)
+	);
+	
+	hex_decoder h2(
+		.hex_digit(p_one_score),
+		.segments(HEX2)
 	);
 
 endmodule
 
-module fsm_controller (resetn, p_one, p_two, start, square, p_one_win, p_two_win, blue, red, turn);
+module fsm_controller (resetn, p_one, p_two, start, square, p_one_win, p_two_win, blue, red, turn, p_one_score, p_two_score);
 	
 	input [5:0] square;
 	input resetn, p_one, p_two, start;
+	output reg [3:0] p_one_score, p_two_score;
 	output reg p_one_win, p_two_win;
 	output reg turn;
-	output reg [19:0] blue, red;
+	output reg [41:0] blue, red;
 	
 	always @(*)
 	begin
@@ -155,6 +170,8 @@ module fsm_controller (resetn, p_one, p_two, start, square, p_one_win, p_two_win
 			p_one_win 	<= 0;
 			p_two_win   <= 0;
 			turn        <= 0;
+			p_one_score <= 4'h0;
+			p_two_score <= 4'h0;
 		end
 		else if (!start)
 		begin
@@ -162,31 +179,101 @@ module fsm_controller (resetn, p_one, p_two, start, square, p_one_win, p_two_win
 			p_two_win <= 0;
 			turn      <= 1;
 		end
-		else if (start)
+		else if (start==1 && p_one_win==0 && p_two_win==0)
 		begin
-			if(!p_one_win && !p_two_win)
+			if (p_one==1)
 			begin
 				case(square)
-					5'b00000: if(turn == 1 && blue[0]==0 && red[0]==0) 	 begin blue[0]<=1;  turn<=0; end
-					5'b00001: if(turn == 1 && blue[1]==0 && red[1]==0) 	 begin blue[1]<=1;  turn<=0; end
-					5'b00010: if(turn == 1 && blue[2]==0 && red[2]==0) 	 begin blue[2]<=1;  turn<=0; end
-					5'b00011: if(turn == 1 && blue[3]==0 && red[3]==0) 	 begin blue[3]<=1;  turn<=0; end
-					5'b00100: if(turn == 1 && blue[4]==0 && red[4]==0) 	 begin blue[4]<=1;  turn<=0; end
-					5'b00101: if(turn == 1 && blue[5]==0 && red[5]==0) 	 begin blue[5]<=1;  turn<=0; end
-					5'b00110: if(turn == 1 && blue[6]==0 && red[6]==0) 	 begin blue[6]<=1;  turn<=0; end
-					5'b00111: if(turn == 1 && blue[7]==0 && red[7]==0) 	 begin blue[7]<=1;  turn<=0; end
-					5'b01000: if(turn == 1 && blue[8]==0 && red[8]==0) 	 begin blue[8]<=1;  turn<=0; end
-					5'b01001: if(turn == 1 && blue[9]==0 && red[9]==0) 	 begin blue[9]<=1;  turn<=0; end
-					5'b01010: if(turn == 1 && blue[10]==0 && red[10]==0) begin blue[10]<=1; turn<=0; end
-					5'b01011: if(turn == 1 && blue[11]==0 && red[11]==0) begin blue[11]<=1; turn<=0; end
-					5'b01100: if(turn == 1 && blue[12]==0 && red[12]==0) begin blue[12]<=1; turn<=0; end
-					5'b01101: if(turn == 1 && blue[13]==0 && red[13]==0) begin blue[13]<=1; turn<=0; end
-					5'b01110: if(turn == 1 && blue[14]==0 && red[14]==0) begin blue[14]<=1; turn<=0; end
-					5'b01111: if(turn == 1 && blue[15]==0 && red[15]==0) begin blue[15]<=1; turn<=0; end
-					5'b10000: if(turn == 1 && blue[16]==0 && red[16]==0) begin blue[16]<=1; turn<=0; end
-					5'b10001: if(turn == 1 && blue[17]==0 && red[17]==0) begin blue[17]<=1; turn<=0; end
-					5'b10010: if(turn == 1 && blue[18]==0 && red[18]==0) begin blue[18]<=1; turn<=0; end
-					5'b10011: if(turn == 1 && blue[19]==0 && red[19]==0) begin blue[19]<=1; turn<=0; end
+					6'b000000: if(turn == 1 && blue[0]==0  && red[0]==0) 	 begin blue[0]<=1;  turn<=0; end
+					6'b000001: if(turn == 1 && blue[1]==0  && red[1]==0) 	 begin blue[1]<=1;  turn<=0; end
+					6'b000010: if(turn == 1 && blue[2]==0  && red[2]==0) 	 begin blue[2]<=1;  turn<=0; end
+					6'b000011: if(turn == 1 && blue[3]==0  && red[3]==0) 	 begin blue[3]<=1;  turn<=0; end
+					6'b000100: if(turn == 1 && blue[4]==0  && red[4]==0) 	 begin blue[4]<=1;  turn<=0; end
+					6'b000101: if(turn == 1 && blue[5]==0  && red[5]==0) 	 begin blue[5]<=1;  turn<=0; end
+					6'b000110: if(turn == 1 && blue[6]==0  && red[6]==0) 	 begin blue[6]<=1;  turn<=0; end
+					6'b000111: if(turn == 1 && blue[7]==0  && red[7]==0) 	 begin blue[7]<=1;  turn<=0; end
+					6'b001000: if(turn == 1 && blue[8]==0  && red[8]==0) 	 begin blue[8]<=1;  turn<=0; end
+					6'b001001: if(turn == 1 && blue[9]==0  && red[9]==0) 	 begin blue[9]<=1;  turn<=0; end
+					6'b001010: if(turn == 1 && blue[10]==0 && red[10]==0) 	 begin blue[10]<=1; turn<=0; end
+					6'b001011: if(turn == 1 && blue[11]==0 && red[11]==0) 	 begin blue[11]<=1; turn<=0; end
+					6'b001100: if(turn == 1 && blue[12]==0 && red[12]==0) 	 begin blue[12]<=1; turn<=0; end
+					6'b001101: if(turn == 1 && blue[13]==0 && red[13]==0) 	 begin blue[13]<=1; turn<=0; end
+					6'b001110: if(turn == 1 && blue[14]==0 && red[14]==0) 	 begin blue[14]<=1; turn<=0; end
+					6'b001111: if(turn == 1 && blue[15]==0 && red[15]==0) 	 begin blue[15]<=1; turn<=0; end
+					6'b010000: if(turn == 1 && blue[16]==0 && red[16]==0) 	 begin blue[16]<=1; turn<=0; end
+					6'b010001: if(turn == 1 && blue[17]==0 && red[17]==0) 	 begin blue[17]<=1; turn<=0; end
+					6'b010010: if(turn == 1 && blue[18]==0 && red[18]==0) 	 begin blue[18]<=1; turn<=0; end
+					6'b010011: if(turn == 1 && blue[19]==0 && red[19]==0) 	 begin blue[19]<=1; turn<=0; end
+					6'b010100: if(turn == 1 && blue[20]==0 && red[20]==0) 	 begin blue[20]<=1; turn<=0; end
+					6'b010101: if(turn == 1 && blue[21]==0 && red[21]==0) 	 begin blue[21]<=1; turn<=0; end
+					6'b010110: if(turn == 1 && blue[22]==0 && red[22]==0) 	 begin blue[22]<=1; turn<=0; end
+					6'b010111: if(turn == 1 && blue[23]==0 && red[23]==0) 	 begin blue[23]<=1; turn<=0; end
+					6'b011000: if(turn == 1 && blue[24]==0 && red[24]==0) 	 begin blue[24]<=1; turn<=0; end
+					6'b011001: if(turn == 1 && blue[25]==0 && red[25]==0) 	 begin blue[25]<=1; turn<=0; end
+					6'b011010: if(turn == 1 && blue[26]==0 && red[26]==0) 	 begin blue[26]<=1; turn<=0; end
+					6'b011011: if(turn == 1 && blue[27]==0 && red[27]==0) 	 begin blue[27]<=1; turn<=0; end
+					6'b011100: if(turn == 1 && blue[28]==0 && red[28]==0) 	 begin blue[28]<=1; turn<=0; end
+					6'b011101: if(turn == 1 && blue[29]==0 && red[29]==0) 	 begin blue[29]<=1; turn<=0; end
+					6'b011110: if(turn == 1 && blue[30]==0 && red[30]==0) 	 begin blue[30]<=1; turn<=0; end
+					6'b011111: if(turn == 1 && blue[31]==0 && red[31]==0) 	 begin blue[31]<=1; turn<=0; end
+					6'b100000: if(turn == 1 && blue[32]==0 && red[32]==0) 	 begin blue[32]<=1; turn<=0; end
+					6'b100001: if(turn == 1 && blue[33]==0 && red[33]==0) 	 begin blue[33]<=1; turn<=0; end
+					6'b100010: if(turn == 1 && blue[34]==0 && red[34]==0) 	 begin blue[34]<=1; turn<=0; end
+					6'b100011: if(turn == 1 && blue[35]==0 && red[35]==0) 	 begin blue[35]<=1; turn<=0; end
+					6'b100100: if(turn == 1 && blue[36]==0 && red[36]==0) 	 begin blue[36]<=1; turn<=0; end
+					6'b100101: if(turn == 1 && blue[37]==0 && red[37]==0) 	 begin blue[37]<=1; turn<=0; end
+					6'b100110: if(turn == 1 && blue[38]==0 && red[38]==0) 	 begin blue[38]<=1; turn<=0; end
+					6'b100111: if(turn == 1 && blue[39]==0 && red[39]==0) 	 begin blue[39]<=1; turn<=0; end
+					6'b101000: if(turn == 1 && blue[40]==0 && red[40]==0) 	 begin blue[40]<=1; turn<=0; end
+					6'b101001: if(turn == 1 && blue[41]==0 && red[41]==0) 	 begin blue[41]<=1; turn<=0; end
+				endcase
+			end
+			else
+			begin
+			p_two_win <= 1;
+				case(square)
+					6'b000000: if(turn == 1 && blue[0]==0  && red[0]==0) 	 begin red[0]<=1;  turn<=0; end
+					6'b000001: if(turn == 1 && blue[1]==0  && red[1]==0) 	 begin red[1]<=1;  turn<=0; end
+					6'b000010: if(turn == 1 && blue[2]==0  && red[2]==0) 	 begin red[2]<=1;  turn<=0; end
+					6'b000011: if(turn == 1 && blue[3]==0  && red[3]==0) 	 begin red[3]<=1;  turn<=0; end
+					6'b000100: if(turn == 1 && blue[4]==0  && red[4]==0) 	 begin red[4]<=1;  turn<=0; end
+					6'b000101: if(turn == 1 && blue[5]==0  && red[5]==0) 	 begin red[5]<=1;  turn<=0; end
+					6'b000110: if(turn == 1 && blue[6]==0  && red[6]==0) 	 begin red[6]<=1;  turn<=0; end
+					6'b000111: if(turn == 1 && blue[7]==0  && red[7]==0) 	 begin red[7]<=1;  turn<=0; end
+					6'b001000: if(turn == 1 && blue[8]==0  && red[8]==0) 	 begin red[8]<=1;  turn<=0; end
+					6'b001001: if(turn == 1 && blue[9]==0  && red[9]==0) 	 begin red[9]<=1;  turn<=0; end
+					6'b001010: if(turn == 1 && blue[10]==0 && red[10]==0) 	 begin red[10]<=1; turn<=0; end
+					6'b001011: if(turn == 1 && blue[11]==0 && red[11]==0) 	 begin red[11]<=1; turn<=0; end
+					6'b001100: if(turn == 1 && blue[12]==0 && red[12]==0) 	 begin red[12]<=1; turn<=0; end
+					6'b001101: if(turn == 1 && blue[13]==0 && red[13]==0) 	 begin red[13]<=1; turn<=0; end
+					6'b001110: if(turn == 1 && blue[14]==0 && red[14]==0) 	 begin red[14]<=1; turn<=0; end
+					6'b001111: if(turn == 1 && blue[15]==0 && red[15]==0) 	 begin red[15]<=1; turn<=0; end
+					6'b010000: if(turn == 1 && blue[16]==0 && red[16]==0) 	 begin red[16]<=1; turn<=0; end
+					6'b010001: if(turn == 1 && blue[17]==0 && red[17]==0) 	 begin red[17]<=1; turn<=0; end
+					6'b010010: if(turn == 1 && blue[18]==0 && red[18]==0) 	 begin red[18]<=1; turn<=0; end
+					6'b010011: if(turn == 1 && blue[19]==0 && red[19]==0) 	 begin red[19]<=1; turn<=0; end
+					6'b010100: if(turn == 1 && blue[20]==0 && red[20]==0) 	 begin red[20]<=1; turn<=0; end
+					6'b010101: if(turn == 1 && blue[21]==0 && red[21]==0) 	 begin red[21]<=1; turn<=0; end
+					6'b010110: if(turn == 1 && blue[22]==0 && red[22]==0) 	 begin red[22]<=1; turn<=0; end
+					6'b010111: if(turn == 1 && blue[23]==0 && red[23]==0) 	 begin red[23]<=1; turn<=0; end
+					6'b011000: if(turn == 1 && blue[24]==0 && red[24]==0) 	 begin red[24]<=1; turn<=0; end
+					6'b011001: if(turn == 1 && blue[25]==0 && red[25]==0) 	 begin red[25]<=1; turn<=0; end
+					6'b011010: if(turn == 1 && blue[26]==0 && red[26]==0) 	 begin red[26]<=1; turn<=0; end
+					6'b011011: if(turn == 1 && blue[27]==0 && red[27]==0) 	 begin red[27]<=1; turn<=0; end
+					6'b011100: if(turn == 1 && blue[28]==0 && red[28]==0) 	 begin red[28]<=1; turn<=0; end
+					6'b011101: if(turn == 1 && blue[29]==0 && red[29]==0) 	 begin red[29]<=1; turn<=0; end
+					6'b011110: if(turn == 1 && blue[30]==0 && red[30]==0) 	 begin red[30]<=1; turn<=0; end
+					6'b011111: if(turn == 1 && blue[31]==0 && red[31]==0) 	 begin red[31]<=1; turn<=0; end
+					6'b100000: if(turn == 1 && blue[32]==0 && red[32]==0) 	 begin red[32]<=1; turn<=0; end
+					6'b100001: if(turn == 1 && blue[33]==0 && red[33]==0) 	 begin red[33]<=1; turn<=0; end
+					6'b100010: if(turn == 1 && blue[34]==0 && red[34]==0) 	 begin red[34]<=1; turn<=0; end
+					6'b100011: if(turn == 1 && blue[35]==0 && red[35]==0) 	 begin red[35]<=1; turn<=0; end
+					6'b100100: if(turn == 1 && blue[36]==0 && red[36]==0) 	 begin red[36]<=1; turn<=0; end
+					6'b100101: if(turn == 1 && blue[37]==0 && red[37]==0) 	 begin red[37]<=1; turn<=0; end
+					6'b100110: if(turn == 1 && blue[38]==0 && red[38]==0) 	 begin red[38]<=1; turn<=0; end
+					6'b100111: if(turn == 1 && blue[39]==0 && red[39]==0) 	 begin red[39]<=1; turn<=0; end
+					6'b101000: if(turn == 1 && blue[40]==0 && red[40]==0) 	 begin red[40]<=1; turn<=0; end
+					6'b101001: if(turn == 1 && blue[41]==0 && red[41]==0) 	 begin red[41]<=1; turn<=0; end
 				endcase
 			end
 		end
@@ -194,19 +281,46 @@ module fsm_controller (resetn, p_one, p_two, start, square, p_one_win, p_two_win
 	
 	always @(*)
 		begin
-			if(blue[0] == 1 && blue[5] == 1 && blue[10] == 1 && blue[15] == 1)
+			if(blue[0] == 1 && blue[6] == 1 && blue[12] == 1 && blue[18] == 1)
 			begin p_one_win <= 1; end
-			else if (blue[0] == 1 && blue[4] == 1 && blue[8] == 1 && blue[12] == 1)
+			else if (blue[6] == 1 && blue[12] == 1 && blue[18] == 1 && blue[24] == 1)
 			begin p_one_win <= 1; end
-			else if (blue[1] == 1 && blue[5] == 1 && blue[9] == 1 && blue[13] == 1)
+			else if (blue[12] == 1 && blue[18] == 1 && blue[24] == 1 && blue[30] == 1)
 			begin p_one_win <= 1; end
-			else if (blue[2] == 1 && blue[6] == 1 && blue[10] == 1 && blue[14] == 1)
+			else if (blue[18] == 1 && blue[24] == 1 && blue[30] == 1 && blue[36] == 1)
 			begin p_one_win <= 1; end
-			else if (blue[3] == 1 && blue[7] == 1 && blue[11] == 1 && blue[15] == 1)
+			else if (blue[0] == 1 && blue[1] == 1 && blue[2] == 1 && blue[3] == 1)
 			begin p_one_win <= 1; end
 			else
 			begin p_one_win <= 0; end
 		end
+	
+	always @(posedge p_one_win)
+		begin
+			p_one_score <= p_one_score + 4'h1;
+		end
+		
+	always @(*)
+		begin
+			if(red[0] == 1 && red[6] == 1 && red[12] == 1 && red[18] == 1)
+			begin p_two_win <= 1; end
+			else if (red[6] == 1 && red[12] == 1 && red[18] == 1 && red[24] == 1)
+			begin p_two_win <= 1; end
+			else if (red[12] == 1 && red[18] == 1 && red[24] == 1 && red[30] == 1)
+			begin p_two_win <= 1; end
+			else if (red[18] == 1 && red[24] == 1 && red[30] == 1 && red[36] == 1)
+			begin p_two_win <= 1; end
+			else if (red[0] == 1 && red[1] == 1 && red[2] == 1 && red[3] == 1)
+			begin p_two_win <= 1; end
+			else
+			begin p_two_win <= 0; end
+		end
+	
+	always @(posedge p_two_win)
+		begin
+			p_two_score <= p_two_score + 4'h1;
+		end
+
 	
 endmodule
 
